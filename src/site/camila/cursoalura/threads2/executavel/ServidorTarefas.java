465fa46df56779a991a763d40c9403d6d9a9951e
cami <camiladsantoscavalcante@gmail.com>
2021-01-09 05:41:28 -0300

    util class 02.02 - Vídeo 2

--- src/site/camila/cursoalura/threads2/executavel/ServidorTarefas.java
+++ src/site/camila/cursoalura/threads2/executavel/ServidorTarefas.java
@@ -2,6 +2,8 @@ package site.camila.cursoalura.threads2.executavel;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import site.camila.cursoalura.threads2.classes.DistribuirTarefas;
 
@@ -9,17 +11,14 @@ public class ServidorTarefas {
 	public static void main(String[] args) throws Exception {
 		System.out.println("--- Iniciando o Servidor ---");
 		ServerSocket servidor = new ServerSocket(1235);
+		ExecutorService threadPool = Executors.newCachedThreadPool();
 		
 		while(true) {
 			Socket socket = servidor.accept();
 			System.out.println("Aceitando novo cliente na porta " + socket.getPort());
 			
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-			Thread threadCliente = new Thread(distribuirTarefas);
-			
-			threadCliente.start();
-			
-			Thread.sleep(2000);
+			threadPool.execute(distribuirTarefas);
 		}
 	}
 }
